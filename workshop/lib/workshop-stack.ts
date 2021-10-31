aa45a9aa61c4462e58a3dfbbe9c41fee72afcfb2
fukushi0294 <kf2821@gmail.com>
2021-11-01 02:41:02 +0900

    init cdk workshop

---
+++ workshop/lib/workshop-stack.ts
@@ -0,0 +1,24 @@
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigw from '@aws-cdk/aws-apigateway';
+import * as cdk from '@aws-cdk/core';
+import { HitCounter } from './hitcounter';
+
+export class WorkshopStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler'
+    });
+
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
+    });
+
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: helloWithCounter.handler
+    });
+  }
+}
