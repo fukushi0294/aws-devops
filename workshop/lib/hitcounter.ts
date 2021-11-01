c0399f9a1f784c525eab5ad0de16a180cc546287
fukushi0294 <kf2821@gmail.com>
2021-11-02 00:07:09 +0900

    add table viewer and complete workshop

--- workshop/lib/hitcounter.ts
+++ workshop/lib/hitcounter.ts
@@ -12,10 +12,12 @@ export class HitCounter extends cdk.Construct {
   /** allows accessing the counter function */
   public readonly handler: lambda.Function;
 
+  public readonly table: dynamodb.Table;
+
   constructor(scope: cdk.Construct, id: string, props: HitCounterProps) {
     super(scope, id);
 
-    const table = new dynamodb.Table(this, 'Hits', {
+    this.table = new dynamodb.Table(this, 'Hits', {
         partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING }
     });
 
@@ -25,11 +27,11 @@ export class HitCounter extends cdk.Construct {
         code: lambda.Code.fromAsset('lambda'),
         environment: {
             DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
-            HITS_TABLE_NAME: table.tableName
+            HITS_TABLE_NAME: this.table.tableName
         }
     });
 
-    table.grantWriteData(this.handler);
+    this.table.grantWriteData(this.handler);
     props.downstream.grantInvoke(this.handler);
   }
 }
--- workshop/lib/workshop-stack.ts
+++ workshop/lib/workshop-stack.ts
@@ -2,6 +2,7 @@ import * as lambda from '@aws-cdk/aws-lambda';
 import * as apigw from '@aws-cdk/aws-apigateway';
 import * as cdk from '@aws-cdk/core';
 import { HitCounter } from './hitcounter';
+import { TableViewer } from 'cdk-dynamo-table-viewer';
 
 export class WorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -19,6 +20,13 @@ export class WorkshopStack extends cdk.Stack {
 
     new apigw.LambdaRestApi(this, 'Endpoint', {
       handler: helloWithCounter.handler
+    });
+    
+    /* use this viewer on only development */
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithCounter.table,
+      sortBy: 'hits'
     });
   }
 }
